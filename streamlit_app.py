ac6941d4f142ba9ca574be2d1cccf767cb1c3faa
Romain Attié <92431813+romainattie@users.noreply.github.com>
2023-06-02 11:01:35 +0200

    Update streamlit_app.py
    color_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+# import color_or_style column in a df
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 df = pd.DataFrame(my_catalog)
 streamlit.write(df)
 
+color_list = df[0].values.tolist()
+print(color_list)
 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
