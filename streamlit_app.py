8950907af91e81bb5f2671815aa0cbeca7e44e5b
Romain Attié <92431813+romainattie@users.noreply.github.com>
2023-06-02 10:51:35 +0200

    Update streamlit_app.py
    my catalog added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,19 @@
 import streamlit
 import snowflake.connector
+import pandas as pd
 
-streamlit.title('Zenas\'s Catalog')
+streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
+# connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
+my_cur.execute("select color_or_style from catalog_for_website")
+my_catalog = my_cur.fetchall()
+df = pd.DataFrame(my_catalog)
+streamlit.write(df)
+
+
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
